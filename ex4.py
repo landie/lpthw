5fecd96985aff3187fb5c1adf8c73a4a56645ce0
Joakim Olsson <laggmonkei@gmail.com>
2013-10-02 19:45:22 +0200

    ex4

---
+++ ex4.py
@@ -0,0 +1,18 @@
+#exercise 4
+
+cars = 100
+space_in_a_car = 4.0
+drivers = 30
+passengers = 90
+cars_not_driven = cars - drivers
+cars_driver = drivers
+carpool_capacity = cars_driver * space_in_a_car
+average_passengers_per_car = passengers /cars_driver
+unused_variable = 500
+
+print "there are", cars , "cars available"
+print "tehre are only ", drivers, "drivers available"
+print "there will be", cars_not_driven, "empty cars today"
+print "we can transport", carpool_capacity, "people today"
+print "we have ", passengers, "to carpool today"
+print "we need to put about ", average_passengers_per_car, "in each car"
